b646823bc918d6139128a0ff1c2f192097602b5a
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-10-09 16:23:07 +0500

    two crystal balls problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jumpAmount;
+
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jumpAmount;
+    let j = i + jumpAmount;
+
+    for (; i <= j && i < breaks.length; i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
