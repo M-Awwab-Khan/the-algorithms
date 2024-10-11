e7a5eefe44035adba30cf57ba12f90fc0e17b4dd
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-10-11 15:13:56 +0500

    stack implemented using singly linked list

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,35 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        this.length++;
+        const head = { value: item, next: this.head } as Node<T>;
+        this.head = head;
+    }
 
-}
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+        const head = this.head;
+        this.head = head.next;
+        this.length--;
 
-}
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
