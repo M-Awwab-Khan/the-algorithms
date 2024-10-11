ef64d254e0b1232f8783524d24e46803b41595cf
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-10-11 21:58:56 +0500

    arraylist implementation in progress

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,60 @@
 export default class ArrayList<T> {
     public length: number;
+    private array: T[];
 
-    
+
 
     constructor() {
+        this.length = 0;
+        this.array = [];
     }
 
     prepend(item: T): void {
-
-}
+        for (let i = this.length - 1; i >= 0; --i) {
+            this.array[i + 1] = this.array[i];
+        }
+        this.array[0] = item;
+        this.length++;
+    }
     insertAt(item: T, idx: number): void {
-
-}
+        for (let i = this.length - 1; i >= idx; --i) {
+            this.array[i + 1] = this.array[i];
+        }
+        this.array[idx] = item;
+        this.length++;
+    }
     append(item: T): void {
-
-}
+        this.array[this.length] = item;
+        this.length++;
+    }
     remove(item: T): T | undefined {
+        let idx = this._search(item);
+        if (idx != -1) {
+            for (let i = idx; i < this.length - 1; ++i) {
+                this.array[i] = this.array[i + 1];
+            }
+            this.length--;
+            return item;
+        }
+        return undefined;
 
-}
+    }
     get(idx: number): T | undefined {
-
-}
+        if (idx >= 0 && idx < this.length) {
+            return this.array[idx];
+        }
+        return undefined;
+    }
     removeAt(idx: number): T | undefined {
 
-}
+    }
+    _search(item: T): number {
+        for (let i = 0; i < this.length; ++i) {
+            if (this.array[i] === item) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
