bd8ad406ca0df6ab48426172c6a8388c63f026e0
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-10-13 19:04:50 +0500

    feat: bfs implemented using queue

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,25 @@
+import Queue from "./Queue";
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    let queue = new Queue<BinaryNode<number>>();
+    queue.enqueue(head);
 
+    while (!queue.isEmpty()) {
+        let curr = queue.deque();
+
+        if (!curr) continue;
+        if (curr.value === needle) {
+            return true;
+        }
+
+        if (curr.left) {
+            queue.enqueue(curr.left);
+        }
+
+        if (curr.right) {
+            queue.enqueue(curr.right);
+        }
+    }
+
+    return false;
 }
+
