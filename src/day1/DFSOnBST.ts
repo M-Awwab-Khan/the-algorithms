c8ae426348c98a79a7ef2ff2dd7c725924ca8c5a
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-10-14 06:42:56 +0500

    feat: dfs implemented

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,19 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function walk(node: BinaryNode<number>, needle: number): boolean {
+  if (!node) {
+    return false;
+  }
+  if (node.value === needle) {
+    return true;
+  }
+  if (needle > node.value) {
+    return walk(node.right, needle);
+  }
+  if (needle <= node.value) {
+    return walk(node.left, needle); 
+  }
+}
 
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return walk(head, needle);
 }
