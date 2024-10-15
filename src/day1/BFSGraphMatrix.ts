1ad66d0d9a70a4a09260f69e3be834ab12c0855b
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-10-15 19:50:25 +0500

    feat: BFS implemented on graph adjacency matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,43 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+import Queue from "./Queue";
 
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    let q = new Queue<number>();
+    let seen: boolean[] = new Array(graph.length).fill(false);
+    let prev: number[] = new Array(graph.length).fill(-1);
+
+    q.enqueue(source);
+
+    while (!q.isEmpty()) {
+        let curr = q.deque() as number;
+
+        if (curr === needle) {
+            break;
+        }
+        let adjs = graph[curr];
+        for (let i = 0; i < adjs.length; ++i) {
+            if (adjs[i] !== 0 && !seen[i]) {
+                seen[i] = true;
+                q.enqueue(i);
+                prev[i] = curr;
+            }
+        }
+    }
+    //lets build back the path
+    if (prev[needle] === -1) {
+        return null;
+    }
+
+    let path: number[] = [];
+    let curr = needle;
+    while (prev[curr] !== -1) {
+        path.push(curr);
+        curr = prev[curr];
+    }
+
+    return [source].concat(path.reverse());
 }
+
